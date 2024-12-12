6f35410e04dcd5f6fddd7e4e8d3caf32caa3c9d1
Akib Mulla <akibmulla47@gmail.com>
2024-12-12 12:27:21 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+window.onload = function () {
+  var blist = document.querySelector(".blist");
+  var scrollLeftButton = document.querySelector(".scroll-left");
+  var scrollRightButton = document.querySelector(".scroll-right");
+
+  scrollLeftButton.addEventListener("click", function () {
+    blist.scrollTo({
+      left: blist.scrollLeft - 370,
+      behavior: 'smooth'
+    });
+  });
+
+  scrollRightButton.addEventListener("click", function () {
+    blist.scrollTo({
+      left: blist.scrollLeft + 370,
+      behavior: 'smooth'
+    });
+  });
+};
